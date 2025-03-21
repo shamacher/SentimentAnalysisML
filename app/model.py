538045f3e6a87b01789ebd4f451429e25f0de04d
shamacher <shamacher2@huskers.unl.edu>
2025-03-21 19:15:41 +0100

    compound is additive, so changed way sentiment is evaluated

--- app/model.py
+++ app/model.py
@@ -7,9 +7,11 @@ sia = SentimentIntensityAnalyzer()
 
 def analyze_sentiment(text):
     sentiment_scores = sia.polarity_scores(text)
-    if sentiment_scores['compound'] >= 0.1:
+    if sentiment_scores['pos'] > sentiment_scores['neg'] and \
+            sentiment_scores['pos'] > sentiment_scores['neu']:
         return 'positive'
-    elif sentiment_scores['compound'] <= -0.1:
+    elif sentiment_scores['neg'] > sentiment_scores['pos'] and \
+            sentiment_scores['neg'] > sentiment_scores['neu']:
         return 'negative'
     else:
         return 'neutral'
